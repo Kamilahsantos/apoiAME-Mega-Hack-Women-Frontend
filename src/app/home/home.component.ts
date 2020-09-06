d7c2ee5beec6749a3e268e6bf635c04e1350af93
kamilah santos <kahsantos89@gmail.com>
2020-09-06 20:41:47 -0300

    busca por endereco funcionandoooooooo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewChild, ElementRef, NgZone} from '@angular/core';
 import { finalize } from 'rxjs/operators';
+import { MapsAPILoader, MouseEvent } from '@agm/core';
 
-import { QuoteService } from './quote.service';
+
 
 @Component({
   selector: 'app-home',
@@ -11,26 +12,86 @@ import { QuoteService } from './quote.service';
 export class HomeComponent implements OnInit {
 
   title: string = 'AGM project';
-  latitude = 40.730610;
+  latitude = 45.730610;
   longitude = -73.935242;
   zoom:number;
+  address: string;
+  private  geoCoder ;
+
+
+@ViewChild('search')
+  public searchElementRef: ElementRef;
+
+   constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) { }
 
 
 
   ngOnInit() {
-    this.setCurrentLocation();
+//load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      this.geoCoder = new google.maps.Geocoder;
+
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement);
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
 
     
   }
+
+
   private setCurrentLocation() {
-    if ('geolocation' in navigator) {
+      if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
         this.latitude = position.coords.latitude;
         this.longitude = position.coords.longitude;
-        this.zoom = 15;
+        this.zoom = 8;
+        this.getAddress(this.latitude, this.longitude);
       });
     }
   }
-  
+
+  markerDragEnd($event: MouseEvent) {
+    console.log($event);
+    this.latitude = $event.coords.lat;
+    this.longitude = $event.coords.lng;
+    this.getAddress(this.latitude, this.longitude);
+  }
+
+  getAddress(latitude, longitude) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results:any, status:any) => {
+      console.log(results);
+      console.log(status);
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 12;
+          this.address = results[0].formatted_address;
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+
+    });
+  }
 
 }
